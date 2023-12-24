0cf6709b694cd7cfd83cea8ffca1b71588c34e32
0bscenity <154705783+0bscenity@users.noreply.github.com>
2023-12-23 21:56:23 -0600

    Update optimized.py

--- optimized.py
+++ optimized.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 from bs4 import BeautifulSoup
 
 def is_person_dead(url, name):
@@ -24,17 +25,18 @@ def is_person_dead(url, name):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-#url list
+# Define the URLs for each person
 netscrape = "https://www.famousbirthdays.com/people/dennis-prager.html"
-bushscrape = 'https://www.famousbirthdays.com/people/george-w-bush.html'
 pragerscrape = "https://www.famousbirthdays.com/people/dennis-prager.html"
 dickscrape = 'https://www.famousbirthdays.com/people/benjamin-netanyahu.html'
+bushscrape = 'https://www.famousbirthdays.com/people/george-w-bush.html'
 hkscrape = "https://www.famousbirthdays.com/people/henry-kissinger.html"
 jpscrape = "https://www.famousbirthdays.com/people/jordan-peterson.html"
 
+# Check if each person is dead or not
 is_person_dead(netscrape, "bn")
-is_person_dead(bushscrape, "bushjr")
 is_person_dead(pragerscrape, "dp")
 is_person_dead(dickscrape, "dick")
+is_person_dead(bushscrape, "bushjr")
 is_person_dead(hkscrape, "hk")
 is_person_dead(jpscrape, "jp")
